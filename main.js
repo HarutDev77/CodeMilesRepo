3cd999017c1326b536852521174dd3e4c0c46a13
harutDev77 <harut155794@gmail.com>
2024-06-18 17:33:52 +0400

    fix copyright

--- main.js
+++ main.js
@@ -8,6 +8,12 @@ const LongLIne = document.querySelectorAll("#LongLine");
 const mobMenufirstDiv = document.querySelector(".identifierFirstDiv");
 const mobMenuSecondDiv = document.querySelector(".identifierSecondDiv");
 const mobMenuThirdDiv = document.querySelector(".identifierThirdDiv");
+const copyright = document.querySelector("#copyright");
+
+const specificDate = new Date();
+const year = specificDate.getFullYear();
+
+copyright.innerText = `© ${year} Code Miles LLC. All rights reserved.`
 
 buttonLeaveRequest !== null
   ? buttonLeaveRequest.addEventListener("click", toggleFeedBackForm)
